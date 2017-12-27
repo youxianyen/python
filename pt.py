186a15ca19d411f66f9ce6341652363f093ae89b
youxianyen <youxianyen@sina.com>
2017-12-27 11:49:31 +0800

    first commit

---
+++ pt.py
@@ -0,0 +1,12 @@
+#-*-coding:utf-8 -*-
+#第一行的#代表使用utf-8编码，防止乱码。本行代表注释
+print ('Hello World!')
+print ('Hello again')
+print ('i like trying this')
+print ('This is fun.')
+print ('Yay!printing')
+print ("I'd rather you 'not'.")
+print ('I "said" do not touch this.')
+print ('哇哈哈')
+print ("Hi # there.")
+print ('Hi # there.')
